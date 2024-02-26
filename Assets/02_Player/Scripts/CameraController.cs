b4131b43d922143c4a5c634c4862faf607bea14f
Ralf Zeilstra <ralfzeils@gmail.com>
2024-02-26 12:28:21 +0100

    Added Cinemachine and New Input System

--- Assets/02_Player/Scripts/CameraController.cs
+++ Assets/02_Player/Scripts/CameraController.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    private PlayerInputActions _actions;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _actions = new PlayerInputActions();
+        _actions.Enable();
     }
 
     // Update is called once per frame
