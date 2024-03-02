c677d4b22a0babb94ee3419c1326cb5353b98da7
Penguinnke <121119907+Penguinnke@users.noreply.github.com>
2024-03-02 20:53:45 +0100

    UI update test

--- Assets/00_Scenes/Nynke's Scenes/Scripts/AnimationScripts/DropDownOption.cs
+++ Assets/00_Scenes/Nynke's Scenes/Scripts/AnimationScripts/DropDownOption.cs
@@ -6,32 +6,21 @@ using UnityEngine.UI;
 public class DropDownOption : MonoBehaviour
 
 {
-    // Reference to the dropdown UI element
     public Dropdown dropdown;
-
-    // Reference to the object you want to turn on
     public GameObject objectToTurnOn;
 
     void Start()
-    {
-        // Subscribe to the dropdown's OnValueChanged event
-        dropdown.onValueChanged.AddListener(delegate {
-            DropdownValueChanged(dropdown);
-        });
+    {        dropdown.onValueChanged.AddListener(delegate {
+            DropdownValueChanged(dropdown);});
     }
-
-    // Method to handle dropdown value change
     void DropdownValueChanged(Dropdown dropdown)
     {
-        // Check if option 1 is selected
-        if (dropdown.value == 0) // Adjust the value according to your dropdown options
+        if (dropdown.value == 0) 
         {
-            // Turn on the object
             objectToTurnOn.SetActive(true);
         }
         else
         {
-            // Turn off the object (optional)
             objectToTurnOn.SetActive(false);
         }
     }
