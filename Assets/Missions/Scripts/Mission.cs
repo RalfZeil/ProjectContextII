836a2507880ac05eaab82a4c39493296e8344c8c
Yoyonova <nova.huijs@student.hku.nl>
2024-03-05 19:08:28 +0100

    Revert "Revert "Added Mission Support""
    This reverts commit e79201e91c43fc75a90919c6ee7398a7d54ba12f.

---
+++ Assets/Missions/Scripts/Mission.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mission : MonoBehaviour
+{
+    public virtual bool IsCompleted()
+    {
+        return false;
+    }
+    public virtual void GetReward()
+    {
+
+    }
+
+}
